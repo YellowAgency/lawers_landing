74978da44722625ccd6eaa5fb3d3c88036113bd9
Danil Akimov <bobbiblabbi@hotmail.com>
2016-10-19 01:50:20 +0300

    поправлены тексты, добавлено модальное окно, пофиксил формы

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+function getConsultation(e){
+    document.getElementById('openModal').style.visibility = 'visible';
+    document.getElementById('openModal').style.opacity = '1'
+}
+
+function closeConsultation(e){
+    document.getElementById('openModal').style.visibility = 'hidden';
+    document.getElementById('openModal').style.opacity = '0'
+}
+
+
+
