b8432d81b09522d47497a638fc269419fd0eecba
sp41mer <sp41mer@yahoo.com>
2016-10-19 00:50:51 +0300

    add validator

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,53 @@
 $(document).ready(function(){
     $('.js__request_form').submit(function(e){
         e.preventDefault();
-        var jsonForAjax = $(this).serialize();
-        $.post( "", jsonForAjax);
-    })
+        $(this).validate({
+            rules:{
+
+                name:{
+                    required: true,
+                    minlength: 4,
+                    maxlength: 16
+                },
+
+                phone_number:{
+                    required: true,
+                    minlength: 6,
+                    maxlength: 16
+                },
+
+                question:{
+                    required: true,
+                    minlength: 4,
+                    maxlength: 16
+                }
+
+            },
+
+            messages:{
+
+                name:{
+                    required: "Это поле обязательно для заполнения",
+                    minlength: "Логин должен быть минимум 4 символа",
+                    maxlength: "Максимальное число символо - 16"
+                },
+
+                phone_number:{
+                    required: "Это поле обязательно для заполнения",
+                    minlength: "Пароль должен быть минимум 6 символа",
+                    maxlength: "Пароль должен быть максимум 16 символов"
+                },
+
+                question:{
+                    required: "Это поле обязательно для заполнения",
+                    minlength: "Пароль должен быть минимум 6 символа",
+                    maxlength: "Пароль должен быть максимум 16 символов"
+                }
+
+            }
+
+         });
+    });
+    //var jsonForAjax = $(this).serialize();
+    //$.post( "", jsonForAjax);
 });
