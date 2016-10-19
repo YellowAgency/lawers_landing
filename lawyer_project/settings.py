f0a9a1558b39604dc508c4200a00a24c8e0722be
maratkanov <maratkanov@yandex-team.ru>
2016-10-19 03:43:13 +0300

    add local db

--- lawyer_project/settings.py
+++ lawyer_project/settings.py
@@ -86,8 +86,13 @@ WSGI_APPLICATION = 'lawyer_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
