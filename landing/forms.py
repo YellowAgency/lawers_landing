7d7f80f7e59858b07b3abe48df5337d9d23fe188
maratkanov <maratkanov@yandex-team.ru>
2016-10-19 03:25:02 +0300

    clean для оставления запроса

--- landing/forms.py
+++ landing/forms.py
@@ -7,3 +7,11 @@ class ClaimForm(forms.ModelForm):
     class Meta:
         model = Claim
         fields = ['client_name', 'phone_number', 'claim_text', 'email']
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        phone_number = cleaned_data.get("phone_number")
+        email = cleaned_data.get("email")
+
+        if not (phone_number or email):
+            raise forms.ValidationError("Требуется заполнить одно из полей")
